d463f40a91712ac2e75aea04a4a59e39ca6390c2
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-01-11 23:28:43 +0700

    add blesh

--- home.nix
+++ home.nix
@@ -27,6 +27,7 @@
     pkgs.starship
     pkgs.neovim
     pkgs.lazygit
+    pkgs.blesh
 
 
     # language-server and formatter
@@ -45,6 +46,7 @@
     ./modules/zoxide.nix
     ./modules/lazygit.nix
     ./modules/git.nix
+    ./modules/blesh.nix
   ];
 
   home.file = {
