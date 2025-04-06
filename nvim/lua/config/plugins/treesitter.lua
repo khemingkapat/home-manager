5e2e52ae8560dec18e5978f6e73286a610c2c0dd
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-04-06 17:28:14 +0700

    add text-boject

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     build = ":TSUpdate", -- when update, we call this
     config = function()
       require 'nvim-treesitter.configs'.setup {
@@ -18,6 +21,25 @@ return {
             end
           end,
           additional_vim_regex_highlighting = true,
+        },
+        indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["ii"] = "@conditional.inner",
+              ["ai"] = "@conditional.outer",
+              ["il"] = "@loop.inner",
+              ["al"] = "@loop.outer",
+            },
+          },
         },
       }
     end
